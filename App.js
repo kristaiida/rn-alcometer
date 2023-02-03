9bae376f33c3fdc2cf03384cbe36855dafc8efdb
kristaiida <92725476+kristaiida@users.noreply.github.com>
2023-02-03 10:52:10 +0200

    init

--- App.js
+++ App.js
@@ -1,37 +1,118 @@
 import { useState } from 'react';
-import { View } from 'react-native';
+import { View, TextInput, Text, Switch, Button, ScrollView, Alert } from 'react-native';
 import NumericInput from 'react-native-numeric-input';
-import SelectDropdown from 'react-native-select-dropdown';
 import Styles from './styles/Styles.js';
-import { RadioButton } from 'react-native-paper';
-
-const products = [
-  {name: "Google Chromecast 4k", price: 77},
-  {name: "Sony WH-1000XM4", price: 299},
-  {name: "JBL Flip 6", price: 79},
-  {name: "Genelec 83330A", price: 899}
-];
-
-const shippingOptions = [
-  {type: 'Fetch from store', price: 0},
-  {type: 'DHL', price: 8},
-  {type: 'UPS', price: 10},
-  {type: 'Post letter', price: 4},
-]
+import { RadioButton, Provider, MD3LightTheme, MD3DarkTheme } from 'react-native-paper';
 
 export default function App() {
 
-  const [quantity, setQuantity] = useState(0);
-  const [product, setProduct] = useState(products[0]);
-  const [shipping, setShipping] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [bottles, setBottles] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [gender, setGender] = useState('male');
+  const [dark, setDark] = useState(false);
+  const [total, setTotal] = useState(0);
+
+  function handlePress() {
+    let litres = bottles * 0.33;
+    let grams = litres * 8 * 4.5;
+    let burning = weight / 10;
+    let gramsLeft = grams - (burning * hours);
+    let answer = 0;
+
+    if (gender === 'male') {
+      answer = gramsLeft / (weight * 0.7);
+    } else {
+      answer = gramsLeft / (weight * 0.6);
+    }
+    setTotal(answer);
+
+    if (answer < 0) {
+      setTotal(0);
+    }
+  
+    if (isNaN(weight)) {
+      Alert.alert('Error message!', 'Please enter weight.')
+    }};
+
+  const toggleTheme = () => {
+    setDark( !dark );
+  }
+
+  const CustomDefaultTheme = {
+    ...MD3LightTheme,
+    colors: {
+      ...MD3LightTheme.colors,
+    }
+  }
+  
+  const CustomDarkTheme = {
+    ...MD3DarkTheme,
+    colors: {
+      ...MD3DarkTheme.colors,
+      background: '#faebf6',
+      text: '#380038',
+    }
+  }
+
+  const theme = dark ? CustomDarkTheme : CustomDefaultTheme;
 
   return (
-    <View style={Styles.container}>
-      <SelectDropdown/>
-      <NumericInput/>
-      <RadioButton.Group>
+    <Provider theme={theme}>
+    <ScrollView theme={theme}>
+    <View style={{flex:1, padding: 30, backgroundColor: theme.colors.background}}>
+      <View style={Styles.switchRow}>
+        <Switch
+          style={Styles.switch}
+          value={dark}
+          onValueChange={ toggleTheme }
+          trackColor={{false: '#767577', true: '#380038'}}
+          thumbColor={dark ? '#6d5869' : '#ffffff'}
+        />
+      </View>
+      <Text style={Styles.header}>Alcometer</Text>
+      <Text style={Styles.head}>Weight</Text>
+      <TextInput 
+        value={weight}
+        style={Styles.textInput}
+        keyboardType='number-pad'
+        onChangeText={v => {setWeight(Number.parseInt(v))}}
+      />
+      <Text style={Styles.head}>Bottles</Text>
+      <View style={Styles.num}>
+      <NumericInput
+        onChange={b => setBottles(b)}
+        value={bottles}
+        rounded
+        minValue={0}
+      />
+      </View>
+      <Text style={Styles.head}>Hours</Text>
+      <View style={Styles.num}>
+      <NumericInput
+        onChange={h => setHours(h)}
+        value={hours}
+        rounded
+        minValue={0}
+      />
+      </View>
+      <RadioButton.Group onValueChange={newValue => setGender(newValue)} value={gender}>
+        <View>
+          <RadioButton value='male'/>
+          <Text>Male</Text>
+        </View>
+        <View>
+        <RadioButton value='female'/>
+          <Text>Female</Text>
+        </View>
       </RadioButton.Group>
+      <Text style={Styles.total}>{total.toFixed(2)}</Text>
+      <View style={Styles.button}>
+        <Button color={'#380038'} title='CALCULATE' onPress={handlePress}/>
+      </View>
     </View>
+    </ScrollView>
+    </Provider>
   );
 }
 
--- styles/Styles.js
+++ styles/Styles.js
@@ -1,14 +1,50 @@
 import { StyleSheet } from "react-native";
 import Constants from "expo-constants";
 
+
 export default StyleSheet.create({
     container: {
         flex:1,
         marginTop: Constants.statusBarHeight+10,
         padding: 30
     },
+    switchRow:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 5,
+        alignSelf: "flex-end"
+    },
+    switch:{
+        alignSelf:"flex-start",
+    },
+    textInput:{
+        borderBottomWidth: 1,
+        padding: 5,
+        marginBottom: 20
+    },
+    header: {
+        fontWeight: 'bold',
+        fontSize: 30,
+        paddingBottom: 30,
+        textAlign: 'center'
+    },
+    head: {
+        fontSize: 18,
+        paddingBottom: 10
+    },
+    num: {
+        paddingBottom: 15
+    },
+    button: {
+        padding: 30
+    },
+    total: {
+        fontWeight: 'bold',
+        fontSize: 45,
+        textAlign: 'center',
+        paddingTop: 20
+    }
 });
-  
 
 
 
